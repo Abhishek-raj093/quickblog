0f5d5ab9be42034ad4dade25f581046288c4ffb9
Abhishek-raj093 <ss0986825@gmail.com>
2025-08-18 10:03:40 +0530

    Completed Frontend

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -63,7 +63,7 @@ const AddBlog = () => {
         <input type="checkbox" checked={isPublished} className='scale-125 cursor-pointer' onChange={e => setIsPublished(e.target.checked)} />
       </div>
 
-      {/* <button type='submit' className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button> */}
+      <button type='submit' className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
       </div>
     </form>
   )
